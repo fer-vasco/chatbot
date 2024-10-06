5c9f56ee4191f1a9b0e065c3e797614fe1620a44
fer-vasco <fernandovasconi@hotmail.com>
2024-10-05 21:14:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,14 @@
 import streamlit as st
 
-# Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 Prueba!")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "Prueba de input y control"
 )
 
 clave = 123
-clave_input = st.text_input("Clave:", type="password")
 
-if clave_input == clave:
-    st.write("Clave Correcta")
-    st.write(clave_input)
+text_input = st.text_input("Clave 👇")
+
+if text_input:
+        st.write("You entered: ", text_input)
 
